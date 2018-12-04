779bcc886e332451da45224d9e271c262683a696
sheymyster <sheysjunk@gmail.com>
2018-12-03 20:56:40 -0600

    Initial react redux setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ColorChanger from './components/testComponent';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Time to start writing code
+        <ColorChanger />
+      </div>
+    );
+  }
+}
+
+
+export default App;
