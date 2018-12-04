7cbef1c048b95ddbd18a2c4abfa4802172d7f88b
sheymyster <33297329+sheymyster@users.noreply.github.com>
2018-12-04 16:31:28 -0600

    Added base components I'll need. Didn't build them yet though

---
+++ src/components/boostScreen.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+class BoostSelectionBox extends Component {
+
+   render() {
+     return (
+       <div>
+       </div>
+     );
+   }
+};
+
+function mapStateToProps(state) {
+  return{}
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoostSelectionBox);
