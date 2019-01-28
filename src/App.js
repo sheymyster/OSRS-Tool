d4e4fbcb9670bc35116458ede7b678e800a2d62b
sheymyster <sheysjunk@gmail.com>
2019-01-27 21:57:57 -0600

    Starting to experiment with doing the layout with Bulma

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import NavigationMenu from './components/navigationMenu/navigationMenu.js';
 import MainScreen from './screens/mainScreen/mainScreen.js';
+import DPSscreen from './screens/bulma-screens/bulma-dps-screen.js';
 import './App.css';
 
+
+/*<div className="Navigation-Menu"><NavigationMenu /></div>
+<div className="Main-Screen"><MainScreen /></div>*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="Navigation-Menu"><NavigationMenu /></div>
-        <div className="Main-Screen"><MainScreen /></div>
+      <div class="hero hero.header hero.footer app-background">
+        <div ><NavigationMenu /></div>
+        <div ><DPSscreen /></div>
       </div>
     );
   }
---
+++ src/screens/bulma-screens/bulma-dps-screen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class DPSscreen extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default DPSscreen;
